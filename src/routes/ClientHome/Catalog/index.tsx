8d06b8b97409a86873180839a2c2011049e4701b
bwk73 <bwkersz@gmail.com>
2023-08-16 20:19:18 -0300

    First back end requests

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -3,8 +3,21 @@ import SearchBar from "../../../components/SearchBar";
 import CatalogCard from "../../../components/CatalogCard";
 import ButtonNextPage from "../../../components/ButtonNextPage";
 import * as productService from '../../../services/product-service';
+import { ProductDTO } from '../../../models/product';
+import { useEffect, useState } from 'react';
 
 export default function Catalog() {
+
+  const [products, setProducts] = useState<ProductDTO[]>([]);
+
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises
+    productService.findAll()
+      .then(response => {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument, @typescript-eslint/no-unsafe-member-access
+        setProducts(response.data.content);
+      });
+  }, []);
 
   return (
     <main>
@@ -13,7 +26,7 @@ export default function Catalog() {
 
         <div className="dsc-catalog-cards dsc-mb20 dsc-mt20">
           {
-            productService.findAll().map(
+            products.map(
               product => <CatalogCard key={product.id} product={product} />
             )
           } 
--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,15 +2,28 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as producService from "../../../services/product-service";
+import * as productService from '../../../services/product-service';
 import { useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
+import { useEffect, useState } from "react";
+import { ProductDTO } from "../../../models/product";
 
 export default function ProductDetails() {
+  
   const params = useParams();
 
-  const product = producService.findById(Number(params.productId));
+  const [product, setProduct] = useState<ProductDTO>();
 
+  useEffect(() => {
+    // eslint-disable-next-line @typescript-eslint/no-floating-promises, @typescript-eslint/restrict-template-expressions
+    productService.findById(Number(params.productId))
+      .then(response => {
+        console.log(response.data);
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+        setProduct(response.data);
+      });
+  }, []);
+  
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
