8343fc1aa6224644d1b32fbf918e7ec0e4f86c0c
bwk73 <bwkersz@gmail.com>
2023-08-31 06:25:48 -0300

    Cart clear

--- src/routes/ClientHome/ProductDetails/index.tsx
+++ src/routes/ClientHome/ProductDetails/index.tsx
@@ -2,11 +2,12 @@ import "./styles.css";
 import ButtonInverse from "../../../components/ButtonInverse";
 import ButtonPrimary from "../../../components/ButtonPrimary";
 import ProductDetailsCard from "../../../components/ProductDetailsCard";
-import * as productService from '../../../services/product-service';
 import { useNavigate, useParams } from "react-router-dom";
 import { Link } from "react-router-dom";
 import { useEffect, useState } from "react";
 import { ProductDTO } from "../../../models/product";
+import * as productService from '../../../services/product-service';
+import * as cartService from '../../../services/cart-service';
 
 export default function ProductDetails() {
   
@@ -29,13 +30,22 @@ export default function ProductDetails() {
         navigate("/");
       });
   }, []);
+
+  function handleBuyClick() {
+    if (product) {
+      cartService.addProduct(product);
+      navigate("/cart");
+    }
+  }
   
   return (
     <main>
       <section id="product-details-section" className="dsc-container">
         {product && <ProductDetailsCard product={product} />}
         <div className="dsc-btn-page-container">
-          <ButtonPrimary text="Comprar" />
+          <div onClick={handleBuyClick}>
+            <ButtonPrimary text="Comprar" />
+          </div>
           <Link to="/">
             <ButtonInverse text="Início" />
           </Link>
