3747d3c4cf868dd30dc8daf5d99c5e7d135393dd
bwk73 <bwkersz@gmail.com>
2023-09-15 09:50:54 -0300

    isAuthenticated

--- src/routes/ClientHome/Catalog/index.tsx
+++ src/routes/ClientHome/Catalog/index.tsx
@@ -5,6 +5,7 @@ import ButtonNextPage from "../../../components/ButtonNextPage";
 import * as productService from '../../../services/product-service';
 import { ProductDTO } from '../../../models/product';
 import { useEffect, useState } from 'react';
+import { isAuthenticated } from '../../../services/auth-service';
 
 type QueryParams = {
   page: number;
@@ -23,7 +24,8 @@ export default function Catalog() {
   const [isLastPage, setIsLastPage] = useState(false);
 
   useEffect(() => {
-
+    console.log("AUTENTICADO", isAuthenticated());
+    
     productService.findPageRequest(queryParams.page, queryParams.name)
       .then(response => {
         const nextPage = response.data.content;
