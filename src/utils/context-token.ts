9704f6498c6954cbc07dfe73c4a537a1d6bd2361
bwk73 <bwkersz@gmail.com>
2023-09-16 06:23:51 -0300

    Context token payload

---
+++ src/utils/context-token.ts
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-empty-function */
+import { createContext } from "react";
+import { AccessTokenPayloadDTO } from "../models/auth";
+
+export type ContextTokenType = {
+    contextTokenPayload: AccessTokenPayloadDTO | undefined;
+    setContextTokenPayload: (accessTokenPayload: AccessTokenPayloadDTO | undefined) => void;
+}
+
+export const ContextToken = createContext<ContextTokenType>({
+    contextTokenPayload: undefined,
+    setContextTokenPayload: () => { }
+});
