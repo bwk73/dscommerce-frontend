8f28705fdb4cfea58d04037884291052fad678ac
bwk73 <bwkersz@gmail.com>
2023-09-21 09:38:24 -0300

    Delete finish

--- src/routes/Admin/ProductListing/index.tsx
+++ src/routes/Admin/ProductListing/index.tsx
@@ -32,6 +32,7 @@ export default function ProductListing() {
 
   const [dialogConfirmationData, setDialogConfirmationData] = useState({
     visible: false,
+    id: 0,
     message: "Tem certeza?"
   });
 
@@ -58,11 +59,25 @@ export default function ProductListing() {
     setDialogInfoData({...dialogInfoData, visible: false});
   }
 
-  function handleDeleteClick() {
-    setDialogConfirmationData({...dialogConfirmationData, visible: true});
+  function handleDeleteClick(productId: number) {
+    setDialogConfirmationData({...dialogConfirmationData, id: productId, visible: true});
   }
 
-  function handleDialogConfirmationAnswer(answer: boolean) {
+  function handleDialogConfirmationAnswer(answer: boolean, productId: number) {
+    if (answer) {
+        productService.deleteById(productId)
+          .then(() => {
+            setProducts([]);
+            setQueryParams({ ...queryParams, page: 0 });
+          })
+          .catch(error => {
+            setDialogInfoData({
+              visible: true,
+              message: error.response.data.error
+            })
+          });
+    }
+
     setDialogConfirmationData({...dialogConfirmationData, visible: false});
   }
 
@@ -106,7 +121,7 @@ export default function ProductListing() {
                   />
                 </td>
                 <td>
-                  <img onClick={handleDeleteClick}
+                  <img onClick={() => handleDeleteClick(product.id)}
                     className="dsc-product-listing-btn"
                     src={deleteIcon}
                     alt="Deletar"
@@ -132,6 +147,7 @@ export default function ProductListing() {
       {
         dialogConfirmationData.visible &&
         <DialogConfirmation 
+          id={dialogConfirmationData.id}
           message={dialogConfirmationData.message} 
           onDialogAnswer={handleDialogConfirmationAnswer} 
         />
