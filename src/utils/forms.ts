d8cc5d22ab625c3d5a478f84306e6436a414c578
bwk73 <bwkersz@gmail.com>
2023-09-21 11:38:19 -0300

    forms toValues

--- src/utils/forms.ts
+++ src/utils/forms.ts
@@ -1,4 +1,15 @@
+/* eslint-disable no-var */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 export function update(inputs: any, name: string, newValue: any) {
 
     return { ...inputs, [name]: { ...inputs[name], value: newValue }};
+    
+}
+
+export function toValues(inputs: any) {
+    const data: any = {};
+    for (var name in inputs) {
+        data[name] = inputs[name].value;
+    }
+    return data;
 }
