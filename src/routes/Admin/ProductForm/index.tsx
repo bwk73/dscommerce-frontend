f1712d02559cb9d51254806d6ede123b8516da6f
bwk73 <bwkersz@gmail.com>
2023-09-21 13:57:18 -0300

    forms updateAll

--- src/routes/Admin/ProductForm/index.tsx
+++ src/routes/Admin/ProductForm/index.tsx
@@ -1,6 +1,53 @@
+import { Link, useParams } from "react-router-dom";
 import "./styles.css";
+import { useEffect, useState } from "react";
+import FormInput from "../../../components/FormInput";
+import * as forms from "../../../utils/forms";
+import * as productService from '../../../services/product-service';
 
 export default function ProductForm() {
+
+  const params = useParams();
+
+  const isEditing = params.productId !== 'create';
+
+  const [formData, setFormData] = useState<any>({
+    name: {
+      value: "",
+      id: "name",
+      name: "name",
+      type: "text",
+      placeholder: "Nome",
+    },
+    price: {
+      value: "",
+      id: "price",
+      name: "price",
+      type: "number",
+      placeholder: "Preço",
+    },
+    imgUrl: {
+      value: "",
+      id: "imgUrl",
+      name: "imgUrl",
+      type: "text",
+      placeholder: "Imagem",
+    },
+  });
+
+  useEffect(() => {
+    if (isEditing) {
+        productService.findById(Number(params.productId))
+          .then(response => {
+              const newFormData = forms.updateAll(formData, response.data);
+              setFormData(newFormData);
+          })
+    }
+  }, []);
+
+  function handleInputChange(event: any) {
+    setFormData(forms.update(formData, event.target.name, event.target.value));
+  }
 
     return (
         <main>
@@ -9,29 +56,32 @@ export default function ProductForm() {
             <form className="dsc-card dsc-form">
               <h2>Dados do produto</h2>
               <div className="dsc-form-controls-container">
-                <div>
-                  <input className="dsc-form-control" type="text" placeholder="Nome" />
-                </div>
-                <div>
-                  <input className="dsc-form-control" type="text" placeholder="Preço" />
-                </div>
-                <div>
-                  <input className="dsc-form-control" type="text" placeholder="Imagem" />
-                </div>
-                <div>
-                  <select className="dsc-form-control dsc-select" required>
-                    <option value="" disabled selected>Categorias</option>
-                    <option value="1">Valor 1</option>
-                    <option value="2">Valor 2</option>
-                  </select>
-                </div>
-                <div>
-                  <textarea className="dsc-form-control dsc-textarea" placeholder="Descrição"></textarea>
-                </div>
+                  <div>
+                      <FormInput
+                          { ...formData.name }
+                          className="dsc-form-control"
+                          onChange={handleInputChange}
+                      />
+                  </div>
+                  <div>
+                      <FormInput
+                          { ...formData.price }
+                          className="dsc-form-control"
+                          onChange={handleInputChange}
+                        />
+                  </div>
+                  <div>
+                      <FormInput
+                          { ...formData.imgUrl }
+                          className="dsc-form-control"
+                          onChange={handleInputChange}
+                      />
+                  </div>
               </div>
-  
               <div className="dsc-product-form-buttons">
-                <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                  <Link to="/admin/products">
+                      <button type="reset" className="dsc-btn dsc-btn-white">Cancelar</button>
+                  </Link>
                 <button type="submit" className="dsc-btn dsc-btn-blue">Salvar</button>
               </div>
             </form>
